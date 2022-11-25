71ad13bb5b32ca59c4b3abc1b9c11a143871c0cb
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-25 15:40:52 +0530

    post view added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostAddComponent } from './post-add/post-add.component';
+import { PostViewComponent } from './post-view/post-view.component';
 
 const myRouter: Routes=[
   {
     path:"",
     component: PostAddComponent
+  },
+  {
+    path:"view",
+    component: PostViewComponent
   }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostAddComponent
+    PostAddComponent,
+    PostViewComponent
   ],
   imports: [
     BrowserModule,
